6f9513d00e3aa31a2ecb114fa5bcfe65edd2aecc
Shivam Gupta <125425181+ershivuu@users.noreply.github.com>
2023-06-02 11:32:14 +0530

    Add files via upload

---
+++ SpringHibernateAnnotDemo/src/main/java/com/universal/main/AppMain.java
@@ -0,0 +1,58 @@
+package com.universal.main;
+
+import java.util.List;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+import com.universal.dao.ProductDao;
+import com.universal.daoimpl.ProductDaoImpl;
+import com.universal.pojo.Product;
+
+public class AppMain {
+
+	public static void main(String[] args) {
+		
+		ApplicationContext context = 
+		new ClassPathXmlApplicationContext("spring.xml");
+		
+		ProductDao daoImpl = 
+				context.getBean("daoImpl",
+		ProductDaoImpl.class);
+		
+//		List<Product> lst = daoImpl.getAllproducts();
+//		lst.forEach(System.out::println);
+		
+//		Product pr = daoImpl.searchProduct(9);
+//		if(pr != null)
+//			System.out.println(pr);
+//		else
+//			System.out.println("No such product found");
+		
+//		Product product = new Product();
+//		product.setPname("sandals");
+//		product.setPrice(1300);
+//		product.setQty(50);
+//		
+//		if(daoImpl.addNewProduct(product))
+//			System.out.println("product added successfully");
+//		else
+//			System.out.println("product insertion failed");
+//		
+//		Product product = new Product();
+//		product.setPid(10);
+//		product.setPname("floaters");
+//		product.setPrice(1500);
+//		product.setQty(10);
+//		
+//		if(daoImpl.updateProduct(product))
+//			System.out.println("product updated");
+//		else
+//			System.out.println("product updation failed");
+//		
+//		if(daoImpl.deleteProduct(10))
+//			System.out.println("product deleted");
+//		else
+//			System.out.println("product deletion failed");
+	}
+}
